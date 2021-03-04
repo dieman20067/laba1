c0f00ed6da918fac0a4d538317780c5dcb596fa9
user1 <user1@A518-06>
2021-03-04 14:39:38 +0300

    START

--- Lab1.py
+++ Lab1.py
@@ -1,1 +1,5 @@
 print("Hello Python from Visual Studio!")
+# Дана строка. Если она начинается на 'abc', то заменить их на 'www', иначе
+# добавить в конец строки "zzz"
+
+text = input('your last word: \n')
